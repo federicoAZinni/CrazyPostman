05c520b7f62411d5048111506c786f942cd29fb3
Federico.Pizia <federico.zinni@pizia.com>
2023-04-29 13:07:45 -0300

    sahkeCamera

---
+++ Assets/Dev/Scripts/ShakeCamera.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+public class ShakeCamera : MonoBehaviour
+{
+    [SerializeField]CinemachineVirtualCamera vcam;
+    CinemachineBasicMultiChannelPerlin noise;
+    private void Awake()
+    {
+        noise = vcam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Crash")) AnimShakeCamera();
+    }
+
+    void AnimShakeCamera()
+    {
+        noise.m_AmplitudeGain = 3;
+        LeanTween.delayedCall(1, () => {
+            LeanTween.value(3, 0, 0.5f).setOnUpdate((value) => { noise.m_AmplitudeGain = value; });
+        });
+    }
+}
