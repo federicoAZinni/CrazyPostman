88db6db6ef3e3a6edcf53be1c7e6d0052edf4851
Exedetor <exedetorheadgehog@gmail.com>
2023-04-29 01:25:20 -0300

    Añadido el script del proyectil

---
+++ Assets/Dev/Scripts/ProjectilLogic.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectilLogic : MonoBehaviour
+{
+    //Aqui va el codigo del paquete lanzado por el vehiculo y sus checkeos de fisicas
+
+}
