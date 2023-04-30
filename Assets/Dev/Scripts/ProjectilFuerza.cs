4836667b32b0d866fd14c9b29a90cbb0f335df49
Federico.Pizia <federico.zinni@pizia.com>
2023-04-30 18:38:01 -0300

    Add Boxes + anim dessapier box

--- Assets/Dev/Scripts/ProjectilFuerza.cs
+++ Assets/Dev/Scripts/ProjectilFuerza.cs
@@ -7,16 +7,21 @@ public class ProjectilFuerza : MonoBehaviour
 
     [SerializeField] Rigidbody r;
     public TypeMail typeMail;
+    [SerializeField] GameObject[] boxes;
 
+    private void Start()
+    {
+        boxes[(int)typeMail].SetActive(true);
+    }
     public void AplicarFuerza(float fuerza, Vector3 direct)
     {
+       
         r.AddForce(direct * fuerza);
         r.AddRelativeTorque(Vector3.one * 25);
         LeanTween.delayedCall(5, () => {
             if (this != null)
             {
-                Debug.Log("Fallo... alto manco");
-                Destroy(this.gameObject);
+                LeanTween.scale(gameObject, Vector3.zero, 0.3f).setEaseInBack().setOnComplete(() => { Destroy(gameObject); });
             } 
         });
     }
